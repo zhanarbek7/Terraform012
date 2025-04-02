ab975c904b1ecf4a937f01bfe5bb7b142a782d67
Zhan <zhanarbek.a7@gmail.com>
2025-04-02 17:26:30 -0500

    Terraform variables 2

--- variables/main.tf
+++ variables/main.tf
@@ -29,10 +29,32 @@ variable "mymap" {
   }
 }
 
+variable "inputname" {
+  type = string
+  description = "Set the name for a VPC"
+}
+
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = var.vpcname
+    Name = var.inputname
   }
 } 
+
+output "vpcid" {
+  value = aws_vpc.myvpc.id
+}
+
+variable "mytuple" {
+  type = tuple([string, number, string])
+  default = [ "cat", 1, "dog" ]
+}
+
+variable "myobject" {
+  type = object({ name = string, port = list(number)})
+  default = {
+    name = "tj"
+    port = [ 0,1 ]
+  }
+}
